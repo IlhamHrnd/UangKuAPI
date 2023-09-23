ee0da95309d6d5c40ef4b5a6baa3d515d70a858c
IlhamHrnd <ilhamhernadi88@gmail.com>
2023-09-23 21:33:46 +0700

    Add AppStandardReferenceItem

---
+++ EbookAPI/Controllers/AppStandardReferenceItemController.cs
@@ -0,0 +1,53 @@
+﻿using EbookAPI.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UangKuAPI.Filter;
+using UangKuAPI.Model;
+
+namespace UangKuAPI.Controllers
+{
+    [Route("[controller]", Name = "AppStandardReferenceItemAPI")]
+    [ApiController]
+    public class AppStandardReferenceItemController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AppStandardReferenceItemController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetStandardID", Name = "GetStandardID")]
+        public async Task<ActionResult<List<AppStandardReferenceItem>>> GetStandardID([FromQuery] AppStandardReferenceItemFilter filter, bool isActive = false)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(filter.StandardReferenceID))
+                {
+                    return BadRequest("StandardReferenceID Is Required");
+                }
+                string isActiveCondition = isActive ? "AND asri.IsActive = 1" : "";
+
+                var query = $@"SELECT asri.StandardReferenceID, asri.ItemID, asri.ItemName,
+                        asri.Note, asri.IsUsedBySystem, asri.IsActive,
+                        asri.LastUpdateDateTime, asri.LastUpdateByUserID
+                        FROM AppStandardReferenceItem AS asri
+                        WHERE asri.StandardReferenceID = '{filter.StandardReferenceID}' {isActiveCondition};";
+
+                var response = await _context.AppStandardReferenceItems.FromSqlRaw(query).ToListAsync();
+
+                if (response == null || response.Count == 0)
+                {
+                    return NotFound("AppStandardReferenceItem Not Found");
+                }
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+    }
+}
---
+++ EbookAPI/Filter/AppStandardReferenceItemFilter.cs
@@ -0,0 +1,17 @@
+﻿namespace UangKuAPI.Filter
+{
+    public class AppStandardReferenceItemFilter
+    {
+        public string? StandardReferenceID { get; set; }
+
+        public AppStandardReferenceItemFilter()
+        {
+            StandardReferenceID = string.Empty;
+        }
+
+        public AppStandardReferenceItemFilter(string? standardID)
+        {
+            StandardReferenceID = string.IsNullOrEmpty(standardID) ? string.Empty : standardID;
+        }
+    }
+}
