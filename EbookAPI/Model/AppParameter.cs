f338d4ca880d9785df550cef64fdfe6a1a39767c
IlhamHrnd <ilhamhernadi88@gmail.com>
2024-02-25 18:53:22 +0700

    Add Field
    - Penambahan kolom baru di DB AppParameter
    - SRControl

--- EbookAPI/Model/AppParameter.cs
+++ EbookAPI/Model/AppParameter.cs
@@ -9,6 +9,7 @@ namespace UangKuAPI.Model
         public string? ParameterID { get; set; }
         public string? ParameterName { get; set; }
         public string? ParameterValue { get; set; }
+        public string? SRControl { get; set; }
         public DateTime? LastUpdateDateTime { get; set; }
         public string? LastUpdateByUserID { get; set; }
         public bool IsUsedBySystem { get; set; }
