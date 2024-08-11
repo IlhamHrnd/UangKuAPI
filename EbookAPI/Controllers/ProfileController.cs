97d5bd84a490ff2cac282fde24d97bb6eb613d91
IlhamHrnd <ilhamhernadi88@gmail.com>
2024-08-11 15:53:44 +0700

    Major Improve #4

--- EbookAPI/Controllers/ProfileController.cs
+++ EbookAPI/Controllers/ProfileController.cs
@@ -3,8 +3,8 @@ using EbookAPI.Encryptor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UangKuAPI.BusinessObjects.Model;
-using UangKuAPI.Filter;
 using UangKuAPI.Helper;
+using UangKuAPI.BusinessObjects.Filter;
 
 namespace UangKuAPI.Controllers
 {
@@ -20,7 +20,7 @@ namespace UangKuAPI.Controllers
         }
 
         [HttpGet("GetPersonID", Name = "GetPersonID")]
-        public async Task<ActionResult<GetProfile>> GetPersonID([FromQuery] ProfileFilter filter)
+        public async Task<ActionResult<Profile>> GetPersonID([FromQuery] ProfileFilter filter)
         {
             try
             {
@@ -28,17 +28,22 @@ namespace UangKuAPI.Controllers
                 {
                     return BadRequest($"PersonID Is Required");
                 }
-                var query = $@"SELECT p.PersonID, p.FirstName, p.MiddleName, p.LastName, p.BirthDate,
-                                p.PlaceOfBirth, p.Photo, p.Address, p.Province, p.City, p.Subdistrict,
-                                p.District, p.PostalCode, p.LastUpdateDateTime, p.LastUpdateByUser
-                                FROM Profile AS p
-                                WHERE p.PersonID = '{filter.PersonID}';";
-                var response = await _context.Profile.FromSqlRaw(query).ToListAsync();
+
+                var response = await _context.Profile
+                    .Select(p => new Profile
+                    {
+                        PersonID = p.PersonID, FirstName = p.FirstName, MiddleName = p.MiddleName, LastName = p.LastName,
+                        PlaceOfBirth = p.PlaceOfBirth, Photo = p.Photo, Address = p.Address, Province = p.Province,
+                        City = p.City, Subdistrict = p.Subdistrict, District = p.District, PostalCode = p.PostalCode,
+                        LastUpdateDateTime = p.LastUpdateDateTime, LastUpdateByUser = p.LastUpdateByUser, BirthDate = p.BirthDate
+                    })
+                    .Where(p => p.PersonID == filter.PersonID)
+                    .ToListAsync();
+
                 if (response == null || response.Count == 0 || !response.Any())
                 {
                     return NotFound("PersonID Not Found");
                 }
-
                 foreach (var item in response)
                 {
                     if (!string.IsNullOrEmpty(item.PersonID))
@@ -55,7 +60,9 @@ namespace UangKuAPI.Controllers
                     }
                 }
 
-                return Ok(response);
+                return response == null || response.Count == 0 || !response.Any()
+                    ? (ActionResult<Profile>)NotFound("Province Not Found")
+                    : (ActionResult<Profile>)Ok(response);
             }
             catch (Exception e)
             {
@@ -64,7 +71,7 @@ namespace UangKuAPI.Controllers
         }
 
         [HttpPost("PostProfile", Name = "PostProfile")]
-        public async Task<IActionResult> PostProfile([FromBody] PostProfile profile)
+        public async Task<IActionResult> PostProfile([FromBody] Profile2 profile)
         {
             try
             {
@@ -72,18 +79,25 @@ namespace UangKuAPI.Controllers
                 {
                     return BadRequest($"Profile Are Required");
                 }
-                else
+
+                var person = await _context.Profile
+                    .Where(p => p.PersonID == profile.PersonID)
+                    .ToListAsync();
+
+                if (person.Any())
                 {
-                    profile.FirstName = EncryptorNullChecker.EncryptIfNotNull(profile.FirstName);
-                    profile.MiddleName = EncryptorNullChecker.EncryptIfNotNull(profile.MiddleName);
-                    profile.LastName = EncryptorNullChecker.EncryptIfNotNull(profile.LastName);
-                    profile.PlaceOfBirth = EncryptorNullChecker.EncryptIfNotNull(profile.PlaceOfBirth);
-                    profile.Address = EncryptorNullChecker.EncryptIfNotNull(profile.Address);
-                    profile.Province = EncryptorNullChecker.EncryptIfNotNull(profile.Province);
-                    profile.City = EncryptorNullChecker.EncryptIfNotNull(profile.City);
-                    profile.District = EncryptorNullChecker.EncryptIfNotNull(profile.District);
-                    profile.Subdistrict = EncryptorNullChecker.EncryptIfNotNull(profile.Subdistrict);
+                    return BadRequest($"{profile.PersonID} Already Exist");
                 }
+
+                profile.FirstName = EncryptorNullChecker.EncryptIfNotNull(profile.FirstName);
+                profile.MiddleName = EncryptorNullChecker.EncryptIfNotNull(profile.MiddleName);
+                profile.LastName = EncryptorNullChecker.EncryptIfNotNull(profile.LastName);
+                profile.PlaceOfBirth = EncryptorNullChecker.EncryptIfNotNull(profile.PlaceOfBirth);
+                profile.Address = EncryptorNullChecker.EncryptIfNotNull(profile.Address);
+                profile.Province = EncryptorNullChecker.EncryptIfNotNull(profile.Province);
+                profile.City = EncryptorNullChecker.EncryptIfNotNull(profile.City);
+                profile.District = EncryptorNullChecker.EncryptIfNotNull(profile.District);
+                profile.Subdistrict = EncryptorNullChecker.EncryptIfNotNull(profile.Subdistrict);
                 string updatedate = DateFormat.DateTimeNow(DateStringFormat.Longyearpattern, DateTime.Now);
                 string date = DateFormat.DateTimeNow(DateStringFormat.Longyearpattern, (DateTime)profile.BirthDate);
 
@@ -94,14 +108,9 @@ namespace UangKuAPI.Controllers
                                 '{profile.City}', '{profile.Subdistrict}', '{profile.District}', '{profile.PostalCode}', '{updatedate}', '{profile.LastUpdateByUser}');";
                 int rowsAffected = await _context.Database.ExecuteSqlRawAsync(query);
 
-                if (rowsAffected > 0)
-                {
-                    return Ok($"Person ID {profile.PersonID} Created Successfully");
-                }
-                else
-                {
-                    return BadRequest($"Failed To Insert Data For Person ID {profile.PersonID}");
-                }
+                return rowsAffected > 0
+                    ? Ok($"Person ID {profile.PersonID} Created Successfully")
+                    : BadRequest($"Failed To Insert Data For Person ID {profile.PersonID}");
             }
             catch (Exception e)
             {
@@ -110,7 +119,7 @@ namespace UangKuAPI.Controllers
         }
 
         [HttpPatch("PatchProfile", Name = "PatchProfile")]
-        public async Task<IActionResult> PatchProfile([FromBody] PostProfile profile)
+        public async Task<IActionResult> PatchProfile([FromBody] Profile2 profile)
         {
             try
             {
@@ -118,49 +127,40 @@ namespace UangKuAPI.Controllers
                 {
                     return BadRequest($"Profile Is Required");
                 }
-                else
+
+                var person = await _context.Profile
+                    .Where(p => p.PersonID == profile.PersonID)
+                    .ToListAsync();
+
+                if (!person.Any())
                 {
-                    profile.FirstName = EncryptorNullChecker.EncryptIfNotNull(profile.FirstName);
-                    profile.MiddleName = EncryptorNullChecker.EncryptIfNotNull(profile.MiddleName);
-                    profile.LastName = EncryptorNullChecker.EncryptIfNotNull(profile.LastName);
-                    profile.PlaceOfBirth = EncryptorNullChecker.EncryptIfNotNull(profile.PlaceOfBirth);
-                    profile.Address = EncryptorNullChecker.EncryptIfNotNull(profile.Address);
-                    profile.Province = EncryptorNullChecker.EncryptIfNotNull(profile.Province);
-                    profile.City = EncryptorNullChecker.EncryptIfNotNull(profile.City);
-                    profile.District = EncryptorNullChecker.EncryptIfNotNull(profile.District);
-                    profile.Subdistrict = EncryptorNullChecker.EncryptIfNotNull(profile.Subdistrict);
+                    return BadRequest($"{profile.PersonID} Not Found");
                 }
-                DateTime dateTime = DateTime.Now;
-                string updatedate = DateFormat.DateTimeNow(DateStringFormat.Longyearpattern, DateTime.Now);
+
+                profile.FirstName = EncryptorNullChecker.EncryptIfNotNull(profile.FirstName);
+                profile.MiddleName = EncryptorNullChecker.EncryptIfNotNull(profile.MiddleName);
+                profile.LastName = EncryptorNullChecker.EncryptIfNotNull(profile.LastName);
+                profile.PlaceOfBirth = EncryptorNullChecker.EncryptIfNotNull(profile.PlaceOfBirth);
+                profile.Address = EncryptorNullChecker.EncryptIfNotNull(profile.Address);
+                profile.Province = EncryptorNullChecker.EncryptIfNotNull(profile.Province);
+                profile.City = EncryptorNullChecker.EncryptIfNotNull(profile.City);
+                profile.District = EncryptorNullChecker.EncryptIfNotNull(profile.District);
+                profile.Subdistrict = EncryptorNullChecker.EncryptIfNotNull(profile.Subdistrict);
+                string updatedate = DateFormat.DateTimeNow(DateStringFormat.Longyearpattern, DateFormat.DateTimeNow());
                 string date = DateFormat.DateTimeNow(DateStringFormat.Longyearpattern, (DateTime)profile.BirthDate);
 
-                var query = $@"UPDATE `Profile`
-                                SET `FirstName` = '{profile.FirstName}',
-                                `MiddleName` = '{profile.MiddleName}',
-                                `LastName` = '{profile.LastName}',
-                                `BirthDate` = '{date}',
-                                `PlaceOfBirth` = '{profile.PlaceOfBirth}',
-                                `Photo` = '{profile.Photo}',
-                                `Address` = '{profile.Address}',
-                                `Province` = '{profile.Province}',
-                                `City` = '{profile.City}',
-                                `Subdistrict` = '{profile.Subdistrict}',
-                                `District` = '{profile.District}',
-                                `PostalCode` = '{profile.PostalCode}',
-                                `LastUpdateDateTime` = '{updatedate}',
-                                `LastUpdateByUser` = '{profile.LastUpdateByUser}'
+                var query = $@"UPDATE `Profile` SET `FirstName` = '{profile.FirstName}', `MiddleName` = '{profile.MiddleName}',
+                                `LastName` = '{profile.LastName}', `BirthDate` = '{date}', `PlaceOfBirth` = '{profile.PlaceOfBirth}',
+                                `Photo` = '{profile.Photo}', `Address` = '{profile.Address}', `Province` = '{profile.Province}',
+                                `City` = '{profile.City}', `Subdistrict` = '{profile.Subdistrict}', `District` = '{profile.District}',
+                                `PostalCode` = '{profile.PostalCode}', `LastUpdateDateTime` = '{updatedate}', `LastUpdateByUser` = '{profile.LastUpdateByUser}'
                                 WHERE `PersonID` = '{profile.PersonID}';";
 
                 var response = await _context.Database.ExecuteSqlRawAsync(query);
 
-                if (response > 0)
-                {
-                    return Ok($"{profile.PersonID} Update Successfully");
-                }
-                else
-                {
-                    return NotFound($"{profile.PersonID} Not Found");
-                }
+                return response > 0 
+                    ? Ok($"{profile.PersonID} Update Successfully") 
+                    : NotFound($"{profile.PersonID} Not Found");
             }
             catch (Exception e)
             {
---
+++ UangKuAPI.BusinessObjects/Filter/ProfileFilter.cs
@@ -0,0 +1,17 @@
+﻿namespace UangKuAPI.BusinessObjects.Filter
+{
+    public class ProfileFilter : Base
+    {
+        public string? PersonID { get; set; }
+
+        public ProfileFilter() : base()
+        {
+            PersonID = string.Empty;
+        }
+
+        public ProfileFilter(int pageNumber, int pageSize, string personID) : base(pageNumber, pageSize)
+        {
+            PersonID = string.IsNullOrEmpty(personID) ? string.Empty : personID;
+        }
+    }
+}
